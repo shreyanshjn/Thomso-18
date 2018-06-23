68f3c9d937fa797c6dcccf246012ccaad5068f83
NikhilM98 <nikhilmehra998@gmail.com>
2018-06-23 22:26:57 +0530

    remove Service wroker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.getRegistrations().then(function(registrations) {
+		for(let registration of registrations) {
+		registration.unregister()
+		}
+	}).catch(function(err) {
+		// console.log(err);
+	});
+} else {
+	// console.log('Service Worker is not supported by browser.');
+}
 $(window).load(function() {
 	new Galaxy($('#galaxy').get(0));
 	$('#galaxy').fadeIn('fast');
