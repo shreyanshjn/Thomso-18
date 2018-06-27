0d1386763948c765fb13b3ce8367517127d688fe
9prashant-1223 <prashantverma1223@gmail.com>
2018-06-27 23:10:31 +0530

    removed files

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,6 @@
-// Flag for enabling cache in production
 var doCache = true;
 
-var CACHE_NAME = 'thomso18beta648648';
+var CACHE_NAME = 'thomso18beta6486482';
 
 // Delete old caches
 self.addEventListener('activate', event => {
@@ -29,10 +28,10 @@ self.addEventListener('install', function(event) {
 			  '/',
 			  '/index.html',
 
-			  '/css/style.css',
               '/css/font.css',
 			  '/css/jquery.fullPage.css',
 			  '/css/style.min.css',
+			  'css/style.css',
 
               '/js/galaxy.js',
 			  '/js/jquery.fullPage.js',
@@ -45,54 +44,6 @@ self.addEventListener('install', function(event) {
 			  '/brochure.html',
 			  '/sponsors.html',
 			  '/sponsorsform.html',
-
-			  '/img/dsouza.jpg',
-			  '/img/economical.png',
-			  '/img/farhan.jpg',
-			  '/img/nucleya.jpg',
-			  '/img/palash.jpg',
-			  '/img/sonu.jpg',
-			  '/img/stats.jpg',
-			  '/img/sunidhi.jpg',
-			  '/img/udit.jpg',
-
-			  '/images/location.png',
-			  '/images/logo.png',
-			  '/images/phone.png',
-			  '/images/web.png',
-			  '/images/background.jpg',
-
-			  '/img/01.jpg',
-			  '/img/02.jpg',
-			  '/img/03.jpg',
-			  '/img/04.jpg',
-			  '/img/06.jpg',
-			  '/img/07.jpg',
-			  '/img/08.jpg',
-			  '/img/09.jpg',
-			  '/img/10.jpg',
-			  '/img/11.jpg',
-			  '/img/12.jpg',
-			  '/img/13.jpg',
-			  '/img/14.jpg',
-			  '/img/15.jpg',
-			  '/img/16.jpg',
-			  '/img/17.jpg',
-			  '/img/18.jpg',
-			  '/img/19.jpg',
-			  '/img/20.jpg',
-			  '/img/21.jpg',
-			  '/img/22.jpg',
-			  '/img/23.jpg',
-			  '/img/24.jpg',
-			  '/img/25.jpg',
-			  '/img/26.jpg',
-			  '/img/27.jpg',
-			  '/img/28.jpg',
-			  '/img/29.jpg',
-			  '/img/30.jpg',
-			  '/img/31.jpg',
-			  '/img/32.jpg'
             ])
 				})
 				.catch(e => console.log(e))
