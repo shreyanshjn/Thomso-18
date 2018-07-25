b126512e29656c4712f6de159ebfdc976db25ef0
prakhar <prakharagarwal21091998@gmail.com>
2018-07-25 22:56:32 +0530

    updated main component ui

---
+++ src/components/home/Main.jsx
@@ -0,0 +1,43 @@
+import React , { Component } from 'react';
+import {SectionsContainer, Section} from 'react-fullpage';
+import logo from '../../img/thomso main-01.png';
+import logoo from '../../img/thomso 18 website CA 1-01.jpg';
+import '../../css/style.css'
+
+
+export default class Main extends Component{
+  render(){
+    let options = {
+      sectionClassName:     'section',
+      anchors:              ['sectionOne', 'sectionTwo', 'sectionThree'],
+      scrollBar:            false,
+      navigation:           false,
+      verticalAlign:        false,
+      sectionPaddingTop:    '0px',
+      sectionPaddingBottom: '50px',
+      slidesNavPosition: 'bottom',
+      arrowNavigation:      true
+    };
+    return(
+      <div className="middlesection">
+      <SectionsContainer {...options}>
+          <Section>
+                <div className="body">
+                  <div className="imagelogo" align="center">
+                  <img src={logo} height="200px"/>
+                  <a href="#sectionTwo" className="scroll-down" address="true"></a>
+                  </div>
+                </div>
+          </Section>
+          <Section>
+                <div id="arrow">
+                page2
+                <img src={logoo} height="250px"/>
+                </div>
+          </Section>
+          <Section>Page 3</Section>
+      </SectionsContainer>
+      </div>
+  );
+  }
+}
