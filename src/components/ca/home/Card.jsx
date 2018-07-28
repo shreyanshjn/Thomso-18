51f01b0791a16ec4468f3b4a7b8abf0ee930d18d
prakhar <prakharagarwal21091998@gmail.com>
2018-07-28 18:01:41 +0530

    updated card ui

--- src/components/ca/home/Card.jsx
+++ src/components/ca/home/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './card.css';
-import logoshare from '../img/share-01.png';
+import './updatedcard.css';
+import logoshare from '../img/icons8-share-50 (1).png';
 export default class Card extends React.Component{
     constructor(){
         super();
@@ -11,26 +11,59 @@ export default class Card extends React.Component{
         };
 
         }
-          toggle() {
+
+        /*  toggle() {
       		this.setState({
       			shown: !this.state.shown, button:!this.state.button
       		});
-      	}
+      	}*/
     render(){
-          var button=this.state.button?"more":"less";
+        /*  var button=this.state.button?"more":"less";
           var shown = {
 			         display: this.state.shown ? "block" : "none"
 		           };
 
 		      var hidden = {
 			         display: this.state.shown ? "none" : "block", lineHeight: '0.8em'
-		           }
+		           }*/
 
         return (
           <div className="maindiv">
             <div style={{width:'356px'}}></div>
             <div className="innerdiv">
-                  <div style={{height:'auto',width:'100%'}}>
+              <div className="wrapper">
+                <div className="card">
+                  	<div className="card__image">
+                  			<img src={this.props.data.full_picture} alt="image" className="border-tlr-radius" />
+                    </div>
+                    <div className="card__content card__padding">
+                          <div className="card__share">
+                            <div className="share-toggle share-icon" onClick={() => this.props.sharePost(this.props.data.id)}> <a><img src={logoshare} alt="fblogo"/></a></div>
+                          </div>
+                          <div className="card__share2">
+                            <div className="share-toggle share-icon">{this.props.data.link ? <a href={this.props.data.link} style={{textDecoration:'none'}} target="_blank" >
+                              <img src={logoshare} alt="fblogo"/> </a> : null}
+                            </div>
+                          </div>
+                          <div className="card__meta">
+                  					<p>posted on {this.props.data.created_time} </p>
+                  				</div>
+                          <div className="card__article">
+                              <p>{this.props.data.message}</p>
+              	    			</div>
+                  	</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          );
+          }
+          }
+
+
+
+                {/*<div style={{height:'auto',width:'100%'}}>
                       {this.props.data.full_picture ? <img src={this.props.data.full_picture} style={{height:'auto',width:'100%',maxHeight:'400px'}} alt={this.props.data.id} /> : null}
                   </div>
                   <div>
@@ -52,10 +85,4 @@ export default class Card extends React.Component{
                       <img src={logoshare} />
                       <p>Share</p></div>
                   </div>
-
-            </div>
-          </div>
-
-        )
-    }
-}
+                    */}
