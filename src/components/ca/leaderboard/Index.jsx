aad198b182c3419b725f0ce6147d08dc6cc195c7
Tushar Varshney <juniorvarshney19@gmail.com>
2018-07-30 23:14:07 +0530

    sidebar hidden in admin

--- src/components/ca/leaderboard/Index.jsx
+++ src/components/ca/leaderboard/Index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import "./style.css"
 import AuthService from '../../../handlers/ca/AuthService';
 import FetchApi from '../../../utils/FetchAPI';
 import '../src/css/Leaderboard.css';
 export default class LeaderboardIndex extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
             users: null,
@@ -12,13 +12,13 @@ export default class LeaderboardIndex extends React.Component {
         this.Auth = new AuthService();
     }
 
-    componentDidMount(){
+    componentDidMount() {
         const authtoken = this.Auth.getToken()
         FetchApi('GET', '/api/ca/leaderboard', null, authtoken)
             .then(r => {
                 console.log(r)
                 if (r && r.data && r.data.length > 0) {
-                    this.setState({users: r.data})
+                    this.setState({ users: r.data })
                 } else {
                     console.log(r)
                 }
@@ -26,56 +26,56 @@ export default class LeaderboardIndex extends React.Component {
             .catch(e => console.log(e));
     }
 
-    render(){
+    render() {
         // const { users } = this.state;
         return (
-            <div>
+            <div className="leader-parent">
                 <h2>Leaderboard</h2>
-            {/* {users ? users.map( (post, index) => {
+                {/* {users ? users.map( (post, index) => {
                     if(post.link) {
                         return <Card key={'CA-Home-Posts'+index} data={post} sharePost={this.sharePost} />
                     }
                     return null;
                 }) : null} */}
                 <div className="maintable">
-                    <table> 
-              <tbody>
-				<tr className="heading">
-					<th>Rank</th>
-					<th>Name</th> 
-					<th>Institute</th>
-					<th className="mobile">Likes</th>
-					<th className="mobile">Shares</th>
-					<th className="mobile">Scores</th>
-				</tr>
-        </tbody>
-            <tbody>
-                {this.state.users ? this.state.users.map((user, index) => {
-                    return (<tr key={`leader${index}`} >
-                    {console.log('test')}
-                    <td>{index+1}</td>
-                    <td>{user.name}</td>
-                    <td>{user.college}</td>
-                    <span className="arrow"></span>
-                    <td className="mobile">{user.likes}</td>
-                    <td className="mobile">{user.shares}</td>
-                    <td className="mobile">{user.score}</td>
-                </tr>)
-                }) : null}
-                </tbody>
-                <tbody>
-				<tr>
-					<td>Rank</td>
-					<td>Name</td> 
-					<td>Institute</td>
-					<td className="mobile">Likes</td>
-					<td className="mobile">Shares</td>
-					<td className="mobile">Scores</td>
-				</tr>
-        </tbody>
-    </table>
+                    <table>
+                        <tbody>
+                            <tr className="heading">
+                                <th>Rank</th>
+                                <th>Name</th>
+                                <th>Institute</th>
+                                <th className="mobile">Likes</th>
+                                <th className="mobile">Shares</th>
+                                <th className="mobile">Scores</th>
+                            </tr>
+                        </tbody>
+                        <tbody>
+                            {this.state.users ? this.state.users.map((user, index) => {
+                                return (<tr key={`leader${index}`} >
+                                    {console.log('test')}
+                                    <td>{index + 1}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.college}</td>
+                                    <span className="arrow"></span>
+                                    <td className="mobile">{user.likes}</td>
+                                    <td className="mobile">{user.shares}</td>
+                                    <td className="mobile">{user.score}</td>
+                                </tr>)
+                            }) : null}
+                        </tbody>
+                        <tbody>
+                            <tr>
+                                <td>Rank</td>
+                                <td>Name</td>
+                                <td>Institute</td>
+                                <td className="mobile">Likes</td>
+                                <td className="mobile">Shares</td>
+                                <td className="mobile">Scores</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-		</div>
-		)
-	}
+        )
+    }
 }
