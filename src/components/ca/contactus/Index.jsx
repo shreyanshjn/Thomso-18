9f2cb6f43d055c4a14e78abd1d1c6a0636bc4530
Tushar Varshney <juniorvarshney19@gmail.com>
2018-07-31 01:09:24 +0530

    contact us

--- src/components/ca/contactus/Index.jsx
+++ src/components/ca/contactus/Index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./css/contactus.css";
+import gmail from "../../beta/home/src/img/gmail.png";
+import call from "../../beta/home/src/img/call.png"
 
 export default class ContactIndex extends React.Component {
     constructor() {
@@ -12,29 +14,97 @@ export default class ContactIndex extends React.Component {
             <div className="contact-parent">
                 <div className="incontact">
                     <div className="incontact_one">
-                        <div className="image_one">
-                        </div>
-                        <div className="details_one">
-                            <div className="name">
-                                <h2>JOHN DOE</h2>
+                        <div className="child flex_column">
+                            <div className="image_one">
                             </div>
-                            <div className="post">
-                                <h2>EVENT MANAGER</h2>
+                            <div className="details_one">
+                                <div className="name">
+                                    <h2>Harshit kanakariya</h2>
+                                </div>
+                                <div className="post">
+                                    <h2>EVENTS AND MANAGEMENT</h2>
+                                </div>
+                                <div className="gmail">
+                                    <img src={gmail} />
+                                    <span>harshit.thomso@gmail.com</span>
+                                </div>
+                                <div className="call">
+                                    <img src={call} />
+                                    <span>+91-9340043505</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="incontact_two">
-                        <div className="image_one">
-                        </div>
-                        <div className="details_one">
-                            <div className="name">
-                                <h2>JOHN DOE</h2>
+                        <div className="child flex_column">
+                            <div className="image_two">
                             </div>
-                            <div className="post">
-                                <h2>EVENT MANAGER</h2>
+                            <div className="details_one">
+                                <div className="name">
+                                    <h2>Rohit Niranjan</h2>
+                                </div>
+                                <div className="post">
+                                    <h2>EVENTS AND MANAGEMENT</h2>
+                                </div>
+                                <div className="gmail">
+                                    <img src={gmail} />
+                                    <span>Niranjan98.thomso@gmail.com</span>
+                                </div>
+                                <div className="call">
+                                    <img src={call} />
+                                    <span>+91-7023511775</span>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    {console.log()}
+                    {(this.props.userData && this.props.userData.gender === "female") ?
+                        <div className="incontact_three">
+                            <div className="child flex_column">
+                                <div className="image_three">
+                                </div>
+                                <div className="details_one">
+                                    <div className="name">
+                                        <h2>Nikhil Mehra</h2>
+                                    </div>
+                                    <div className="post">
+                                        <h2>Technical Head</h2>
+                                    </div>
+                                    <div className="gmail">
+                                        <img src={gmail} />
+                                        <span>nikhil.thomso@gmail.com</span>
+                                    </div>
+                                    <div className="call">
+                                        <img src={call} />
+                                        <span>+91-9116891112</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <div className="incontact_three">
+                            <div className="child flex_column">
+                                <div className="image_four">
+                                </div>
+                                <div className="details_one">
+                                    <div className="name">
+                                        <h2>Prashant Verma</h2>
+                                    </div>
+                                    <div className="post">
+                                        <h2>Technical Head</h2>
+                                    </div>
+                                    <div className="gmail">
+                                        <img src={gmail} />
+                                        <span>prashant.thomso@gmail.com</span>
+                                    </div>
+                                    <div className="call">
+                                        <img src={call} />
+                                        <span>+91-8840592467</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         );
