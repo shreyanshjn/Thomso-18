04130ac751989b632efda355dadd5cd503bd41c8
shreyanshjn <shrey.umt2017@iitr.ac.in>
2018-08-03 02:17:15 +0530

    leaderboard rank corrected

--- src/components/ca/leaderboard/Index.jsx
+++ src/components/ca/leaderboard/Index.jsx
@@ -39,37 +39,36 @@ export default class LeaderboardIndex extends React.Component {
                     return null;
                 }) : null} */}
                 <div className="ca-leader-maintable">
-                    <table> 
+                    <table className="ca-leaderboard-table"> 
                         <tbody>
-                            <tr className="heading">
+                            <tr className="ca-leader-heading">
                                 <th>Rank</th>
                                 <th>Name</th> 
                                 <th>Institute</th>
-                                <th className="mobile">Likes</th>
-                                <th className="mobile">Shares</th>
-                                <th className="mobile">Scores</th>
+                                <th className="ca-leader-mobile">Likes</th>
+                                <th className="ca-leader-mobile">Shares</th>
+                                <th className="ca-leader-mobile">Scores</th>
                             </tr>
-                        </tbody>
-                        <tbody>
                             {this.state.users ? this.state.users.map((user, index) => {
                                 return <DataRow key={`leader${index}`} data={user} index={index} />
                             }) : null}
-                        </tbody>
-                        <tbody>
+
+
+                            {/* user rank*/}
                             {this.props.userData ?
-                            <tr>
+                            <tr className="ca-leader-rank">
                                 <th>rank</th>
                                 <th>{this.props.userData.name ? this.props.userData.name : null}</th> 
-                                <th className="downarrows" onClick={() => this.setState({isExpanded: !this.state.isExpanded})}>{this.props.userData.college ? this.props.userData.college : null}</th>
-                                <th className="mobile">{this.props.userData.likes ? this.props.userData.likes : 0}</th>
-                                <th className="mobile">{this.props.userData.shares ? this.props.userData.shares : 0}</th>
-                                <th className="mobile">{this.props.userData.score ? this.props.userData.score : 0}</th>
+                                <th className="ca-leader-downarrows" onClick={() => this.setState({isExpanded: !this.state.isExpanded})}>{this.props.userData.college ? this.props.userData.college : null}</th>
+                                <th className="ca-leader-mobile">{this.props.userData.likes ? this.props.userData.likes : 0}</th>
+                                <th className="ca-leader-mobile">{this.props.userData.shares ? this.props.userData.shares : 0}</th>
+                                <th className="ca-leader-mobile">{this.props.userData.score ? this.props.userData.score : 0}</th>
                             </tr> : null}
                             {this.state.isExpanded ?
-                                <tr>
-                                    <td className="desktop">0</td>
-                                    <td className="desktop">0</td>
-                                    <td className="desktop">0</td>
+                                    <tr className="ca-leader-rank-mobile">
+                                    <td className="ca-leader-desktop">{this.props.userData.likes ? `Likes ${this.props.userData.likes}` :`Likes 0`}</td>
+                                    <td className="ca-leader-desktop">{this.props.userData.shares ? `Shares ${this.props.userData.shares}` :`Shares 0`}</td>
+                                    <td className="ca-leader-desktop">{this.props.userData.score ? `Score ${this.props.userData.score}` :`Score 0` }</td>
                                 </tr>
                             : null}
                         </tbody>
