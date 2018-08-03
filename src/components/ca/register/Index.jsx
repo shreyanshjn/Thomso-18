b0b64d296e882ef7e92afc136f0a56e0ba82f62a
prakhar <prakharagarwal21091998@gmail.com>
2018-08-04 02:48:02 +0530

    updated register form

--- src/components/ca/register/Index.jsx
+++ src/components/ca/register/Index.jsx
@@ -5,7 +5,7 @@ import AuthService from '../../../handlers/ca/AuthService';
 import FetchApi from '../../../utils/FetchAPI';
 import validateInput from '../../../utils/validation/loginValidation';
 import { Link } from 'react-router-dom' ;
-
+const maxLength=10;
 const options = [
     { value: 'Arunachal Pradesh', label: 'Arunachal Pradesh' },
     { value: 'Assam', label: 'Assam' },
@@ -79,7 +79,11 @@ export default class RegisterIndex extends React.Component {
 
     onChange = (e) => {
         const name = e.target.name;
-        const value = e.target.value;
+        let value = e.target.value;
+        if (name === 'contact' && value) {
+          value = value.trim()
+          value = value.substring(0, 10)
+        }
         this.setState({ [name]: value });
     }
 
@@ -102,7 +106,7 @@ export default class RegisterIndex extends React.Component {
                 .catch(e => {
                     console.log(e)
                 });
-        } else if (check.errors) {
+        } else if (check.errors !== {}) {
             this.setState({ errors: check.errors })
         } else {
             this.setState({ errors: 'Fields cannot be empty' })
@@ -126,12 +130,12 @@ export default class RegisterIndex extends React.Component {
                     </div>
                     <div className="register-form">
                         <form onSubmit={this.onSubmit}>
-                            {/* {errors ?
+                            {errors ?
                                 <div>
                                     {errors}
                                 </div>
                                 : null
-                            } */}
+                            }
                             <div className="form-heading">
                                 <h2>CA Registration form</h2>
                             </div>
@@ -148,17 +152,17 @@ export default class RegisterIndex extends React.Component {
                                         autoComplete="off"
                                         autoCapitalize="on"
                                         onChange={this.onChange}
+                                        spellcheck="false"
                                         required
                                     />
                                 </div>
                                 <div className="form-contactnumber">
                                     <label htmlFor="inputContact">Contact Number</label>
-                                    <input
+                                  <input
                                         id="inputContact"
                                         type="number"
                                         placeholder="Contact Number"
                                         name="contact"
-                                        maxLength="10"
                                         autoCorrect="off"
                                         autoComplete="off"
                                         autoCapitalize="on"
@@ -177,9 +181,11 @@ export default class RegisterIndex extends React.Component {
                                         placeholder="Your Email"
                                         name="email"
                                         autoCorrect="off"
+                                        autoComplete="off"
                                         autoCapitalize="off"
                                         value={email}
                                         onChange={this.onChange}
+                                        spellcheck="false"
                                         required
                                     />
                                 </div>
