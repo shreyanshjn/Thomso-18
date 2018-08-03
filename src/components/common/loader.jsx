c5248592a03f4f3297e40572d3b09470a45c9733
Harsh Vardhan <hvss007@gmail.com>
2018-08-03 22:44:09 +0530

    loader updated

--- src/components/common/loader.jsx
+++ src/components/common/loader.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import "./loader.css" ;
 export default class Loader extends React.Component{
     render(){
-        return(<div className="loadercontainer">
-        <div className="dash uno"></div>
-        <div className="dash dos"></div>
-        <div className="dash tres"></div>
-        <div className="dash cuatro"></div>
-      </div>)
+        return(
+            <div className="loader"></div>
+
+              )
     }
 
 
