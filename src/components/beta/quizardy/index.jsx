b4133562166d9a9d1a97da6eff2686a3384d6bc4
prakhar <prakharagarwal21091998@gmail.com>
2018-08-11 20:23:30 +0530

    added marvel quizadry

--- src/components/beta/quizardy/index.jsx
+++ src/components/beta/quizardy/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SectionsContainer, Section } from 'react-fullpage';
+
 import './quizardy.css';
 import arrow from '../home/src/img/arrow.svg';
 import Instructions from './Instructions';
@@ -14,42 +14,22 @@ export default class QuizardyIndex extends React.Component{
 
         }
     }
-    componentWillMount() {
-        window.location.hash = "#home";
-    }
+
     render(){
-        let options = {
-            sectionClassName: 'section',
-            anchors: ['home', 'aboutUs', 'footfall', 'celebrity', 'contactUs'],
-            scrollBar: false,
-            navigation: false,
-            verticalAlign: false,
-            sectionPaddingTop: '0px',
-            slidesNavPosition: 'bottom',
-            arrowNavigation: true
-        };
+
         return(
             <div>
                 <div className="quizardy-mobile">
                     <Navbar />
                 </div>
-                <SectionsContainer {...options}>
-                    <Section>
                         <div className="quizardy-parent">
+                        <Navbar />
                             <div className="quizardy-child">
-                                <div className="body">
                                     <div className="quizardy-flexcenter">
                                         <a href="https://dare2compete.com/o/quizardry-thomso-iit-roorkee-indian-institute-of-technology-iit-roorkee-64347" target="_blank" rel="noopener noreferrer"><button className="quizardy-button quizardy-opening-button">Dare2Complete</button></a>
-                                        <a href="#aboutUs" address="true">
-                                            <img src={arrow} className="quizardy-opening-downarrow" alt="a" />
-                                        </a>
                                     </div>
-                                </div>
                             </div>
                         </div>
-                    </Section>
-
-                    <Section>
                         <div>
                         <div className='quizardy-desktop'>
                             <Navbar />
@@ -61,8 +41,7 @@ export default class QuizardyIndex extends React.Component{
                           </div>
                         </div>
                         </div>
-                    </Section>
-                </SectionsContainer>
+
             </div>
         );
     }
