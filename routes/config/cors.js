faf107dc83ceb7310270b53cb058fe2364b17130
9prashant-1223 <prashantverma1223@gmail.com>
2018-08-17 03:47:21 +0530

    cross origin fixed

--- routes/config/cors.js
+++ routes/config/cors.js
@@ -1,17 +1,21 @@
-// Whitelist
-var whitelist = ['https://thomso.in', 'https://www.thomso.in', 'www.thomso.in', 'thomso.in',undefined];
-
-if (process.env.REACT_APP_SERVER_ENVIORNMENT === 'dev') {
-  whitelist = ['http://localhost:'+process.env.PORT, 'https://localhost:'+process.env.PORT, 'http://localhost:'+process.env.REACT_APP_SERVER_PORT, 'https://localhost:'+process.env.REACT_APP_SERVER_PORT, 'http://localhost:80', undefined];
-}
 var corsOptions = {
-    origin: function (origin, callback) {
- if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin: function(req, origin, callback) {
+
+    var corsWhitelist = ['https://thomso.in', 'https://www.thomso.in', 'www.thomso.in', 'thomso.in'];
+
+	if (process.env.REACT_APP_SERVER_ENVIORNMENT === 'dev') {
+	  corsWhitelist = ['http://localhost:'+process.env.PORT, 'https://localhost:'+process.env.PORT, 'http://localhost:'+process.env.REACT_APP_SERVER_PORT, 'https://localhost:'+process.env.REACT_APP_SERVER_PORT, 'http://localhost:80'];
+	}
+    if (corsWhitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else if (origin === undefined && ALLOWED_HOSTS.indexOf(req.header.host)) {
+      callback(null, true);
+    } else {
+      // callback(null, true)
+      callback(new Error(' not allowed by CORS'));
     }
-};
+  },
+  credentials: true,
+}
 
 module.exports = corsOptions;
