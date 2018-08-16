3ef21bf664d56c1255cd05b1399c9d7db0041334
9prashant-1223 <prashantverma1223@gmail.com>
2018-08-17 04:46:13 +0530

    removed undefined croos origin from server

--- routes/config/cors.js
+++ routes/config/cors.js
@@ -1,21 +1,18 @@
-var corsOptions = {
-  origin: function(req, origin, callback) {
+// Whitelist
 
-    var corsWhitelist = ['https://thomso.in', 'https://www.thomso.in', 'www.thomso.in', 'thomso.in'];
+ var whitelist = ['https://thomso.in', 'https://www.thomso.in', 'www.thomso.in', 'thomso.in'];
 
-	if (process.env.REACT_APP_SERVER_ENVIORNMENT === 'dev') {
-	  corsWhitelist = ['http://localhost:'+process.env.PORT, 'https://localhost:'+process.env.PORT, 'http://localhost:'+process.env.REACT_APP_SERVER_PORT, 'https://localhost:'+process.env.REACT_APP_SERVER_PORT, 'http://localhost:80'];
-	}
-    if (corsWhitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else if (origin === undefined && ALLOWED_HOSTS.indexOf(req.header.host)) {
-      callback(null, true);
-    } else {
-      // callback(null, true)
-      callback(new Error(' not allowed by CORS'));
-    }
-  },
-  credentials: true,
+if (process.env.REACT_APP_SERVER_ENVIORNMENT === 'dev') {
+  whitelist = ['http://localhost:'+process.env.PORT, 'https://localhost:'+process.env.PORT, 'http://localhost:'+process.env.REACT_APP_SERVER_PORT, 'https://localhost:'+process.env.REACT_APP_SERVER_PORT, 'http://localhost:80'];
+}
+var corsOptions = function (req, callback) {
+  var corsOptions1;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions1 = { origin: true } 
+  }else{
+    corsOptions1 = { origin: false } 
+  }
+  callback(null, corsOptions1) 
 }
 
 module.exports = corsOptions;
