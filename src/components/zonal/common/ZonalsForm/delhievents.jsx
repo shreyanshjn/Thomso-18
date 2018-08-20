677c2ca2a67f78099ff0ba5a7888a03fb46fd429
Tushar Varshney <juniorvarshney19@gmail.com>
2018-08-20 21:45:25 +0530

    dasboard of participants

--- src/components/zonal/common/ZonalsForm/delhievents.jsx
+++ src/components/zonal/common/ZonalsForm/delhievents.jsx
@@ -9,7 +9,6 @@ export default class DelhiEvents extends React.Component {
             selectedOptionTgt: '',
             check: false,
             tgt: false,
-            mrmsthomso: false
         }
     }
     changeState = () => {
@@ -23,9 +22,6 @@ export default class DelhiEvents extends React.Component {
         }
         if (this.state.tgt && this.state.selectedOptionTgt) {
             events.push(this.state.selectedOptionTgt)
-        }
-        if (this.state.mrmsthomso) {
-            events.push('Mr Mrs Thomso')
         }
         this.props.selectedevents(events)
     }
@@ -49,7 +45,7 @@ export default class DelhiEvents extends React.Component {
         return (
             <React.Fragment>
                 <form className={this.props.var ? "register-zonals-form-events inactive" : "register-zonals-form-events active"} onSubmit={this.onSubmit}>
-                        {this.props.errors ? <p className="form-submitted-popup">{this.props.errors}</p> : <p className="form-submitted-popup-hide">&nbsp;</p>}
+                    {this.props.errors ? <p className="form-submitted-popup">{this.props.errors}</p> : <p className="form-submitted-popup-hide">&nbsp;</p>}
                     <div className={this.props.var ? "register-zonals-form-events-parent inactive" : "register-zonals-form-events-parent"}>
                         <div className="register-zonals-form-events-firstchild">
                             <div className="register-zonals-form-events-drama">
@@ -57,10 +53,10 @@ export default class DelhiEvents extends React.Component {
                             </div>
                             <div className={this.state.check ? "register-zonals-form-events-dramachild" : "register-zonals-form-events-dramachild low-opacity"}>
                                 <div>
-                                    <span><input onChange={this.handleOptionChangeDrama} type="radio" name="abhiviyakti" value="abhiviyakti" checked={this.state.selectedOptionDrama === 'abhiviyakti'} disabled={!this.state.check} /></span><span>Abhivyakti</span>
+                                    <span><input onChange={this.handleOptionChangeDrama} type="radio" name="mr_ms_thomso" value="mr_ms_thomso" checked={this.state.selectedOptionDrama === 'mr_ms_thomso'} disabled={!this.state.check} /></span><span>Mr & Miss Thomso</span>
                                 </div>
                                 <div>
-                                    <span><input onChange={this.handleOptionChangeDrama} type="radio" name="natak" value="nukkadnatak" checked={this.state.selectedOptionDrama === 'nukkadnatak'} disabled={!this.state.check} /></span><span>Natak</span>
+                                    <span><input onChange={this.handleOptionChangeDrama} type="radio" name="natak" value="natak" checked={this.state.selectedOptionDrama === 'natak'} disabled={!this.state.check} /></span><span>Natak</span>
                                 </div>
                             </div>
                         </div>
@@ -78,11 +74,6 @@ export default class DelhiEvents extends React.Component {
                                 <div>
                                     <span><input onChange={this.handleOptionChangeTgt} type="radio" name="openmic" value="openmic" checked={this.state.selectedOptionTgt === 'openmic'} disabled={!this.state.tgt} /></span><span>Open mic</span>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="register-zonals-form-events-secondchild">
-                            <div className="register-zonals-form-events-tgt">
-                                <span><input type="checkbox" onClick={() => this.setState({ mrmsthomso: !this.state.mrmsthomso })} name="mrmsthomso" value="mrmsthomso" /></span><span>Mr & Ms Thomso</span>
                             </div>
                         </div>
                     </div>
