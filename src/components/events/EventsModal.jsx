c35eb981117a57a6ca403862a708b716e59ee53e
Tushar Varshney <juniorvarshney19@gmail.com>
2018-08-28 21:25:34 +0530

    events updated

--- src/components/events/EventsModal.jsx
+++ src/components/events/EventsModal.jsx
@@ -102,7 +102,7 @@ export default class EventsModal extends React.Component {
                         </div>
                         {this.state.data ? <EventDetail history={this.props.history} id={this.state.subEventId} detail={this.state.data} eventsId={this.state.eventId} subevents={this.state.data.subevents} data={this.props.data} /> : null}
                     </div>
-                    <div className="events-modals-last-arrow" style={{ display: "flex", justifyContent: "space-between", marginTop: "5px" }}>
+                    <div className="events-modals-last-arrow">
                         <p href="" className="arrow-button-events" onClick={() => this.prevCat(this.state.eventId)}>
                             <img src="/img/main/events/leftarrow.png" alt="left-arrow" width="15px" />
                         </p>
