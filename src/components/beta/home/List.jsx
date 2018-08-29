1636b98c0344ef3d67c0ee968f23384e52e12de8
shreyanshjn <shrey.umt2017@iitr.ac.in>
2018-08-30 02:33:01 +0530

    added campus clicks page

--- src/components/beta/home/List.jsx
+++ src/components/beta/home/List.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom';
+import {Link} from "react-router-dom";
 import "./src/css/List.css";
 
 class List extends Component {
@@ -8,13 +8,13 @@ class List extends Component {
         this.state={
             activeStateLink: window.location.pathname.substring(1)
         };
-        this.setActiveLink=this.setActiveLink.bind(this)
+        this.setActiveLink=this.setActiveLink.bind(this);
     }
     setActiveLink(state)
     {
         this.setState({
             activeStateLink:state
-        })
+        });
 
     }
     render() { 
@@ -23,68 +23,76 @@ class List extends Component {
                 <li>
                     <Link to="../campusambassador" className={(this.state.activeStateLink
                         === "list-ca-link") ? "list-ca-link" : null } 
-                        onClick={()=>{
-                            this.setActiveLink("list-ca-link")
-                        }}>
+                    onClick={()=>{
+                        this.setActiveLink("list-ca-link");
+                    }}>
                         CA PORTAL
                     </Link>
                 </li>
                 {/*       <li>
           <a href="">ZONALS</a>
         </li>*/}
-        <li>
-            <Link to="../quizardry" className={(this.state.activeStateLink === "quizardry") ? "list-quiz-link" : null}
-                onClick={()=>{
-                this.setActiveLink("quizardry")
-                }}>
+                <li>
+                    <Link to="../quizardry" className={(this.state.activeStateLink === "quizardry") ? "list-quiz-link" : null}
+                        onClick={()=>{
+                            this.setActiveLink("quizardry");
+                        }}>
                 QUIZARDRY
-            </Link>
-        </li>
-        {/*<li>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/campusclicks" className={(this.state.activeStateLink === "campusclicks") ? "list-quiz-link" : null}
+                        onClick={()=>{
+                            this.setActiveLink("campusclicks");
+                        }}>
+                CAMPUS CLICKS
+                    </Link>
+                </li>
+                {/*<li>
           <a href="">OFFINE EVENTS</a>
         </li>*/}
-        {/* <li>
+                {/* <li>
           <a href="">TEAM CONTACT</a>
         </li>*/}
-        <li>
-            <Link to="./associate" className={(this.state.activeStateLink === "linkAssociate") ? "list-spons-link" : null }
-                onClick={()=>{
-                this.setActiveLink("linkAssociate")
-                }}>
+                <li>
+                    <Link to="./associate" className={(this.state.activeStateLink === "linkAssociate") ? "list-spons-link" : null }
+                        onClick={()=>{
+                            this.setActiveLink("linkAssociate");
+                        }}>
                 ASSOCIATE WITH US
-            </Link>
-        </li>
-        <li>
-            <Link to="./main" className={(this.state.activeStateLink === "main") ? "list-spons-link" : null }
-                onClick={()=>{
-                this.setActiveLink("main")
-                }}>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="./main" className={(this.state.activeStateLink === "main") ? "list-spons-link" : null }
+                        onClick={()=>{
+                            this.setActiveLink("main");
+                        }}>
                 ASSOCIATE WITH US
-            </Link>
-        </li>
-        <li>
-            <Link to="../../zonals/delhi" className={(this.state.activeStateLink === "zonals-delhi") ? "list-zonals-link" : null }
-                onClick={()=>{
-                this.setActiveLink("list-zonals-delhi")
-                }}>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="../../zonals/delhi" className={(this.state.activeStateLink === "zonals-delhi") ? "list-zonals-link" : null }
+                        onClick={()=>{
+                            this.setActiveLink("list-zonals-delhi");
+                        }}>
                 DELHI-ZONALS
-            </Link>
-         </li>
-         <li>
-            <Link to="../../zonals/lucknow" className={(this.state.activeStateLink === "zonals-lucknow") ? "list-zonals-lucknow" : null }
-                onClick={()=>{
-                this.setActiveLink("list-zonals-lucknow")
-                }}>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="../../zonals/lucknow" className={(this.state.activeStateLink === "zonals-lucknow") ? "list-zonals-lucknow" : null }
+                        onClick={()=>{
+                            this.setActiveLink("list-zonals-lucknow");
+                        }}>
                 LUCKNOW-ZONALS
-            </Link>
-        </li>
+                    </Link>
+                </li>
 
-        {/* <li>
+                {/* <li>
           <a href="">FAQs</a>
         </li>*/}
-      </ul>
-    );
-  }
+            </ul>
+        );
+    }
 }
 
 export default List;
--- src/components/beta/home/Navbar.jsx
+++ src/components/beta/home/Navbar.jsx
@@ -66,15 +66,33 @@ class Navbar extends Component {
                 </Link>
                 </li> */}
                                 <li className="dropdown">
-                                    <Link to="" className={(this.state.activeState === "quizardry") ? "linkEvents" : null}
+                                    <Link to="#" className={(this.state.activeState === "#") ? "linkEvents" : null}
                                         onClick={() => {
-                                            this.setActive("quizardry");
-                                        }}>ONLINE EVENTS</Link>
+                                            this.setActive("#");
+                                        }}>EVENTS
+                                    </Link>
                                     <div className="beta-navbar-dropdown-content">
-                                        <Link to="../quizardry" className={(this.state.activeState === "quizardry") ? "linkEventson" : null}
+                                        <Link to="#" className="events-online-navbar-option">
+                                           ONLINE EVENTS
+                                        </Link>
+                                        <Link to="../quizardry" style={{marginLeft:"15px"}} className={(this.state.activeState === "quizardry") ? "linkEventson" : null}
                                             onClick={() => {
                                                 this.setActive("quizardry");
-                                            }}>QUIZARDRY</Link>
+                                            }}>Quizardry
+                                        </Link>
+                                        <Link to="/campusclicks" style={{marginLeft:"15px"}}  className={(this.state.activeState === "campusclicks") ? "linkEventson" : null}
+                                            onClick={() => {
+                                                this.setActive("campusclicks");
+                                            }}>Campus Clicks
+                                        </Link>
+                                        <Link to="#" className="events-offline-navbar-option">
+                                            OFFLINE EVENTS
+                                        </Link>
+                                        <Link to="/events"  style={{marginLeft:"15px"}} className={(this.state.activeState === "events") ? "linkEvents" : null}
+                                            onClick={() => {
+                                                this.setActive("events");
+                                            }}>Events
+                                        </Link>
                                         {/*  <Link to="" className={(this.state.activeState === "linkEventsoff") ? "linkEventsoff" : null}
                     onClick={() => {
                                     this.setActive("linkEventsoff");
@@ -97,7 +115,8 @@ class Navbar extends Component {
                                     <Link to="../../main" className={(this.state.activeState === "main") ? "linkSponsors" : null}
                                         onClick={() => {
                                             this.setActive("main");
-                                        }}>PARTICIPATE</Link>
+                                        }}><span className="zonals-navbar-option">PARTICIPATE</span><span className="zonals-navbar-new">new</span></Link>
+
                                 </li>
                                 <li className="dropdown">
                                     <Link to="" className={(this.state.activeState === "lucknow") ? "linkLucknow" : null}
