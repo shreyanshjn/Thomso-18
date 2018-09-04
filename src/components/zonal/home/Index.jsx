0f83e50ec0f9cad77ffbbb031bbe03f6c006d456
9prashant-1223 <prashantverma1223@gmail.com>
2018-09-04 19:37:04 +0530

    zonals in main added

--- src/components/zonal/home/Index.jsx
+++ src/components/zonal/home/Index.jsx
@@ -17,8 +17,8 @@ export default class HomeIndex extends Component {
                         <div className="zonals-mainpage-cards-title">
                             Delhi Zonals
                         </div>
-                        <div className="zonals-mainpage-cards-button">
-                                Successfully Organized.
+                        <div className="zonals-mainpage-cards-text">
+                                SUCCESSFULLY ORGANIZED.
                         </div>
                     </div>
                     <div className="zonals-mainpage-cards">
@@ -29,8 +29,48 @@ export default class HomeIndex extends Component {
                         <div className="zonals-mainpage-cards-title">
                             Lucknow Zonals
                         </div>
-                        <div className="zonals-mainpage-cards-button">
-                            Successfully Organized.
+                        <div className="zonals-mainpage-cards-text">
+                                SUCCESSFULLY ORGANIZED.
+                        </div>
+                    </div>
+                </div>
+                <div className="zonals-mainpage-parent-child">
+                    <div className="zonals-mainpage-cards">
+                        {/* <Link to="/zonals/jaipur"> */}
+                            <div className="zonals-mainpage-cards-image-jaipur">
+                            </div>
+                        {/* </Link> */}
+                        <div className="zonals-mainpage-cards-title">
+                            Jaipur Zonals
+                        </div>
+                        <div className="zonals-mainpage-cards-text">
+                                COMING SOON
+                        </div>
+                    </div>
+                    <div className="zonals-mainpage-cards">
+                    {/* <Link to="/zonals/chandigadh"> */}
+                        <div className="zonals-mainpage-cards-image-chandigadh">
+                        </div>
+                    {/* </Link> */}
+                        <div className="zonals-mainpage-cards-title">
+                            Chandigadh Zonals
+                        </div>
+                        <div className="zonals-mainpage-cards-text">
+                            COMING SOON
+                        </div>
+                    </div>
+                </div>
+                <div className="zonals-mainpage-parent-child">
+                    <div className="zonals-mainpage-cards">
+                        {/* <Link to="/zonals/bhopal"> */}
+                            <div className="zonals-mainpage-cards-image-bhopal">
+                            </div>
+                        {/* </Link> */}
+                        <div className="zonals-mainpage-cards-title">
+                            Bhopal Zonals
+                        </div>
+                        <div className="zonals-mainpage-cards-text">
+                                COMING SOON
                         </div>
                     </div>
                 </div>
