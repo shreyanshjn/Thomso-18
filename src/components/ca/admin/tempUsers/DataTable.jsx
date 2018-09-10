d499414662a3c657db1ee4c73353e75637215396
prakhar <prakharagarwal21091998@gmail.com>
2018-09-10 18:33:23 +0530

    quizardry added

--- src/components/ca/admin/tempUsers/DataTable.jsx
+++ src/components/ca/admin/tempUsers/DataTable.jsx
@@ -44,7 +44,7 @@ export default class DataTable extends React.Component {
             });
         });
     }
-    
+
     download = () => {
         if (this.state.participants && this.state.participants.length > 0) {
             downloadCSV({data: this.state.participants, filename: 'temp_registrations.csv'})
@@ -84,4 +84,3 @@ export default class DataTable extends React.Component {
       );
     }
   }
-  
