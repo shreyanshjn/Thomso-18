7e8a14155e6cf9e2ae3aee974386b3117b204fea
prakhar2109 <prakharagarwal21091998@gmail.com>
2018-09-26 13:19:25 +0530

    card modified

--- src/components/beta/team/Index.jsx
+++ src/components/beta/team/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './cards.css';
-import logo from './anushka.jpg';
+import logo from './virat.jpg';
 import fblogo from './img/fbicon.png';
 import inlogo from './img/igicon.png';
 export default class TeamIndex extends React.Component{
@@ -10,18 +10,166 @@ export default class TeamIndex extends React.Component{
             <div className="card-maindiv">
                 <div className="wrapper">
                     <div className="card-inner">
-                        <img src={logo} alt="aaa" className="card-inner-img"/>
-                            <div className="text glow">anushka <br/>sharma</div>
-                            <div className="overlay">
-                                <div className="info">email<br/>aaaaaaaaaa<br/>mobile no<br/>8888888888</div>
-                                <div className="card-footer-icons">
-                                    <div className="card-footer-mainIcons">
-                                        <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
-                                        <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
-                                    </div> 
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
                                 </div>
-                            </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
                     </div>
+
+                    <div className="card-inner">
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
+                    </div>
+
+
+                    <div className="card-inner">
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="card-inner">
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
+                    </div>                    
+                    
+                </div> 
+
+
+
+
+
+                <div className="wrapper">
+            
+                    <div className="card-inner">
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
+                    </div>
+
+
+                    <div className="card-inner">
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
+                    </div>
+
+
+
+                    <div className="card-inner">
+                        <div className="card-innerdiv">
+                                <img src={logo} alt="aaa" className="card-inner-img"/>
+                                <div className="text glow">virat<br/>&nbsp; &nbsp; &nbsp; &nbsp; kohli</div>
+                                <div className="overlay">
+                                    <div className="card-footer-icons">
+                                        <div className="card-footer-mainIcons">
+                                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                        </div> 
+                                    </div>
+                                </div>
+                                <div className="overlay-email">
+                                    <div className="info">email<br/>aaaaaaaaaa<br/><br/>mobile no<br/>8888888888</div>
+                                </div>
+                        </div>
+                        <div className="card-inner-bottom">
+                        <div className="card-position">ACTOR</div>
+                        </div>
+                    </div>                    
+                    
                 </div> 
             </div>
         </div>
