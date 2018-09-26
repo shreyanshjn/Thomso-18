46e4431a74d24ee24a28cb3470f193c97894eb6d
prakhar2109 <prakharagarwal21091998@gmail.com>
2018-09-26 10:36:25 +0530

    changes in card

--- src/components/beta/team/Index.jsx
+++ src/components/beta/team/Index.jsx
@@ -6,46 +6,25 @@ import inlogo from './img/igicon.png';
 export default class TeamIndex extends React.Component{
     render(){
         return (
-            <div>
+        <div>
             <div className="card-maindiv">
                 <div className="wrapper">
                     <div className="card-inner">
-                        <img src={logo} alt="aaa"/>
-                        <div className="text">Anushka Sharma</div>
-                        <div className="overlay">
-                            <div className="info">email<br/>aaaaaaaaaa<br/>Contact<br/>8888888888</div>
-                            <div className="card-footer-mainIcons">
-                            <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
-                            <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
-                       
-                    </div> 
-                        </div>
+                        <img src={logo} alt="aaa" className="card-inner-img"/>
+                            <div className="text glow">anushka <br/>sharma</div>
+                            <div className="overlay">
+                                <div className="info">email<br/>aaaaaaaaaa<br/>mobile no<br/>8888888888</div>
+                                <div className="card-footer-icons">
+                                    <div className="card-footer-mainIcons">
+                                        <div className="card-footer-fbdiv">  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></a></div>
+                                        <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                    </div> 
+                                </div>
+                            </div>
                     </div>
-
-
-                    <div className="card-inner">
-                    </div>
-                    <div className="card-inner">
-                    </div>
-                </div>
-                <div className="wrapper">
-                    <div className="card-inner">
-                    </div>
-                    <div className="card-inner">
-                    </div>
-                    <div className="card-inner">
-                    </div>
-                </div>
-                <div className="wrapper">
-                    <div className="card-inner">
-                    </div>
-                    <div className="card-inner">
-                    </div>
-                    <div className="card-inner">
-                    </div>
-                </div>
+                </div> 
             </div>
-            </div>
+        </div>
         )
     }
 }
