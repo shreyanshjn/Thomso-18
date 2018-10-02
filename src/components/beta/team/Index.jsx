05bdb07fc9b9edd1c95013c490f55e7f66f3ab50
prakhar2109 <prakharagarwal21091998@gmail.com>
2018-10-02 05:36:18 +0530

    team page modified

--- src/components/beta/team/Index.jsx
+++ src/components/beta/team/Index.jsx
@@ -27,35 +27,39 @@ export default class TeamIndex extends React.Component{
                 {this.state.memberdetails.map(post=>
                     <div className="wrapperinnerdiv">
                         {(post && post.members) ? post.members.map(member => 
-                            <div className="card-inner">
-                                <div className="card-innerdiv">
-                                    
+                           <React.Fragment>
+                               {member.image?
+                                <div className="card-inner">
+                                    <div className="card-innerdiv">
+                                        
                                     <img src={`/img/main/team/${member.image}`} alt="aaa"  className="card-inner-img"/>
-                                    
-                                    <div className="text glow">{member.first_name}<br/>&nbsp; &nbsp; &nbsp; &nbsp;{member.last_name}</div>
-                                    
-                                    <div className="overlay-email">
-                                        <div className="info">email<br/>{member.email}<br/><br/>mobile no<br/>{member.mobile}</div>
-                                    </div>
-                                    <div className="overlay">
-                                        <div className="card-footer-icons">
-                                            <div style={{
-                                                zIndex:"200000"
-                                            }} className="card-footer-mainIcons">
-                                                {member.instalink ?  <a href="https://www.facebook.com/thomsoiitroorkee/" target="_blank" rel="noopener noreferrer"> <div className="card-footer-fbdiv">  <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></div></a>: null}
-                                                {member.instalink ? <div className="card-footer-instadiv"><a href="https://www.instagram.com/thomso.IITR/" target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
-                                                : null}
-                                                {member.instalink ?<div className="card-footer-linkediv"><a href="https://www.linkedin.com/company/thomso-iit-roorkee-official" target="_blank" rel="noopener noreferrer"> <img src={ldlogo} className="card-footer-iconsImage" alt="lilogo" /></a></div>: null}
-                                
-                                            </div> 
+                                        
+                                        <div className="text glow">{member.first_name}<br/>&nbsp; &nbsp; &nbsp; &nbsp;{member.last_name}</div>
+                                        
+                                        <div className="overlay-email">
+                                            <div className="info">email<br/>{member.email}<br/><br/>mobile no<br/>{member.mobile}</div>
                                         </div>
+                                        <div className="overlay">
+                                            <div className="card-footer-icons">
+                                                <div style={{
+                                                    zIndex:"200000"
+                                                }} className="card-footer-mainIcons">
+                                                    {member.fblink ?  <a href={member.fblink} target="_blank" rel="noopener noreferrer"> <div className="card-footer-fbdiv">  <img src={fblogo} className="card-footer-iconsImage" alt="fblogo" /></div></a>: null}
+                                                    {member.instalink ? <div className="card-footer-instadiv"><a href={member.instalink} target="_blank" rel="noopener noreferrer"><img src={inlogo} className="card-footer-iconsImage" alt="inlogo" /></a></div>
+                                                    : null}
+                                                    {member.linkedlnlink ?<div className="card-footer-linkediv"><a href={member.linkedlnlink} target="_blank" rel="noopener noreferrer"> <img src={ldlogo} className="card-footer-iconsImage" alt="lilogo" /></a></div>: null}
+                                    
+                                                </div> 
+                                            </div>
+                                        </div>
+                                    
                                     </div>
-                                
+                                    <div className="card-inner-bottom">
+                                        <div className="card-position">{member.position}</div>
+                                    </div>
                                 </div>
-                                <div className="card-inner-bottom">
-                                    <div className="card-position">{member.position}</div>
-                                </div>
-                            </div>
+                                : null}
+                            </React.Fragment>
                         ) : null}
                     </div>
                 )}
