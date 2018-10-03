8bb9ba6957eeab70c17ae61c03c41d32715c3da6
shreyanshjn <shrey.umt2017@iitr.ac.in>
2018-10-03 21:44:40 +0530

    made some changes in workshops and team page

--- src/components/main/workshops/Workshops.js
+++ src/components/main/workshops/Workshops.js
@@ -2,13 +2,13 @@ export const Workshops = [
     {
         name: 'Internet of things',
         image: "IOT.jpeg",
-        description: 'A breakthrough in the modern era - Artificial intelligence and Machine learning are gateways that lead to a whole new dimension of technology. Being great growling concepts with immense potential in the world of technology, Thomso went down the line to organise workshops on AI and ML, yet proving again that Thomso is much more than a cultural festival.',
+        description: 'If you have the zeal and enthusiasm to explore new technologies, Thomso provides you an opportunity to get more actively involved in this learning process with the organisation of another workshop on IoT. Satiate your thirst for technical knowledge by introducing yourself to the amazing world of IOT and its fascinating applications',
         price: "1000"
     },
     {
         name: 'Artificial Intelligence',
         image: "artificial.jpeg",
-        description: 'If you have the zeal and enthusiasm to explore new technologies, Thomso provides you an opportunity to get more actively involved in this learning process with the organisation of another workshop on IoT. Satiate your thirst for technical knowledge by introducing yourself to the amazing world of IOT and its fascinating applications.',
+        description: 'A breakthrough in the modern era - Artificial intelligence and Machine learning are gateways that lead to a whole new dimension of technology. Being great growling concepts with immense potential in the world of technology, Thomso went down the line to organise workshops on AI and ML, yet proving again that Thomso is much more than a cultural festival.',
         price: "1000"
     },
     {
@@ -25,19 +25,19 @@ export const Workshops = [
     },
     {
         name: "Inhouse Photography",
-        image: "photography1.jpeg",
+        image: "photography1.jpg",
         description: "The magic in a photograph is created by the eyes behind the lens. Participants will be taught the intricacies of photography by experts under the roof. Experts will teach you the details and particulars of how to take a photograph." ,
         price: "500"
     },
     {
         name: "OutHouse Photography",
-        image: "photography2.jpeg",
+        image: "photography2 .jpg",
         description:"Spotting and capturing the perfect shot is an art and to learn the nuances of this art, Thomso is organising workshop for enthusiasts to experience comprehensive learning of the photography skills in the real time. Participants will be taken out in the open space for live photography of nature.",
         price: "1500"
     },
     {
         name: "Fitness",
-        image: "fitness.jpeg",
+        image: "fitness.jpg",
         description:"Being fit isn’t a fad or trend, it’s a lifestyle. Realising the importance of this fact, Thomso brings to you a fitness workshop conducted by Rujuta Diwekar, a renowned fitness expert who has trained celebrities like Kareena Kapoor Khan. Thomso brings to you an exclusive chance to get mentored by such a personality.",
         price: "Free"
     }
