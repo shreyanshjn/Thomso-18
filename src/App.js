71459dad8e603b88fabd91061847fb4562c86885
shreyanshjn <shrey.umt2017@iitr.ac.in>
2018-10-16 14:40:32 +0530

    modified faq page

--- src/App.js
+++ src/App.js
@@ -165,10 +165,10 @@ const Toppr = Loadable({
     loading: () => <Loader />
 })
 
-const Ngo = Loadable({
-    loader: () => import("./components/ngo/Index.jsx"),
-    loading: () => <Loader />
-})
+// const Ngo = Loadable({
+//     loader: () => import("./components/ngo/Index.jsx"),
+//     loading: () => <Loader />
+// })
 
 // const Carousel = Loadable({
 //     loader: () => import("./components/carousel/Index"),
@@ -224,7 +224,7 @@ class App extends Component {
                             <Route path="/events" component={Events} />
                             <Route path="/meme" component={Meme} />
                             <Route path="/toppr" component={Toppr} />
-                            <Route path="/ngo" component={Ngo} />
+                            {/* <Route path="/ngo" component={Ngo} />*/}
                             <Route path="/MrMissThomso" component={MrMissThomso} />
                             <Route path="/Payment" component={PaymentIndex} />
                             {/* <Route path="/c" component={Carousel} /> */}
